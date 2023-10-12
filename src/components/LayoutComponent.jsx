42b8e1ab531c96d143177e7b1061bdf6d1b2bc32
GyHuba <gy.huba@gmail.com>
2023-10-12 14:28:29 +0200

    layout component finished

--- src/components/LayoutComponent.jsx
+++ src/components/LayoutComponent.jsx
@@ -1,17 +1,14 @@
 import ContentComponent from "./ContentComponent";
 import FooterComponent from "./FooterComponent";
-import "../css/global.css"
+import "../css/global.css";
 
-
-export default function LayoutComponent(){
-    
-
-    return(
-        <>
-        <div className="game-screen">
+export default function LayoutComponent() {
+  return (
+    <>
+      <div className="game-screen">
         <ContentComponent />
         <FooterComponent />
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
