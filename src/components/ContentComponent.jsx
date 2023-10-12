38467a72f223b27666a08bb263ab012b4f81d26c
GyHuba <gy.huba@gmail.com>
2023-10-12 14:29:20 +0200

    content component added isgameover

--- src/components/ContentComponent.jsx
+++ src/components/ContentComponent.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import CardComponent from "./CardComponent";
+import GameStartComponent from "./GameStartComponent";
+import RestartGameComponent from "./RestartGameComponent";
 
 export default function ContentComponent() {
   const [characterData, setCharacterData] = useState(null);
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [page, setPage] = useState(1);
+  const [isGammeStarted, setIsGameStarted] = useState(false);
+  const [isGameOver, setIsGameOver] = useState(false);
 
   function incrementPage() {
     setPage((page) => page + 1);
@@ -23,28 +27,37 @@ export default function ContentComponent() {
 
   return (
     <>
-      <div className="game-board">
-        <div className="title">Rick and Memorty</div>
-        <div className="scores">
-          <div className="score">
-            Score:{score}
+      {!isGammeStarted && (
+        <GameStartComponent setIsGameStarted={setIsGameStarted} />
+      )}
+      {isGammeStarted && (
+        <div className="game-board">
+          <div className="title">Rick and Memorty</div>
+          <div className="scores">
+            <div className="score">Score:{score}</div>
+            <div className="best-score">Best Score:{bestScore}</div>
           </div>
-          <div className="best-score">
-            Best Score:{bestScore}
-          </div>              
-        </div>
-        {characterData && (
-          <CardComponent
-            cards={characterData}
-            score={score}
-            setScore={setScore}
-            bestScore={bestScore}
-            setBestScore={setBestScore}
-            incrementPage={incrementPage}
-          />
-        )}
-      </div>
 
+          {isGameOver && (
+            <RestartGameComponent
+              setIsGameOver={setIsGameOver}
+              score={score}
+              setScore={setScore}
+            />
+          )}
+          {!isGameOver && characterData && (
+            <CardComponent
+              cards={characterData}
+              score={score}
+              setScore={setScore}
+              bestScore={bestScore}
+              setBestScore={setBestScore}
+              incrementPage={incrementPage}
+              setIsGameOver={setIsGameOver}
+            />
+          )}
+        </div>
+      )}
     </>
   );
 }
